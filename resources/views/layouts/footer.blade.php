61dde20b1d787ed18346ed91b9b592a13d9e6b32
LoaaElsayed <you@example.com>
2024-06-25 16:27:52 +0300

    update permision

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 	<div class="main-footer ht-40">
 		<div class="container-fluid pd-t-0-f ht-100p">
 			<span>Copyright © 2023 <a href="#">InstaOrder Dashboard</a>. Developer by
-            <a href="#">all safe
+            <a href="https://www.allsafeeg.com/en">all safe
 		</div>
 	</div>
 <!-- Footer closed -->
