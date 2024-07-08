f9103a7ef8db0855f2bd2bf9c10606815d694f59
LoaaElsayed <loaaelsayed6@gmail.com>
2024-07-08 13:27:01 +0300

    middleware

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'localhost'
+        'http://localhost:3000',
+        'http://localhost'
     ];
 }
