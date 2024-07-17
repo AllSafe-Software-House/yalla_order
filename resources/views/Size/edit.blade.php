3d595ce35c06255d683af6fbc61025065d7b9d1a
LoaaElsayed <loaaelsayed6@gmail.com>
2024-07-17 16:11:07 +0300

    edits

--- resources/views/Size/edit.blade.php
+++ resources/views/Size/edit.blade.php
@@ -48,7 +48,7 @@
         <div class="mb-3">
             <label for="product_id" class="py-2">Product Name:</label>
             <select id="product_id" class="form-control" name="menue_id">
-                <option value="{{ $data->product_id }}">{{ $data->product->name }}</option>
+                <option value="{{ $data->menue_id }}">{{ $data->menue->product->name }}</option>
             </select>
         </div>
         <div class="mb-3">
