6f936a76dd7d596ca001c08388a54ab4f9b1a3cb
AhmedGamalAttia <ahmed.g.attia169@gmail.com>
2024-08-06 10:43:07 +0300

    Insta Order  handle user wallet and cash back  06-08-2024 v1.3

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -238,7 +238,8 @@ class OrderController extends Controller
             $order = $this->orderdata($integration_id, $ordernum, $ifram_id);
             $orderdetails->update(['payment_order_id'=>$order['id']]);
         }
-        $datauser = $this->datauser($integration_id, $order['amount_cents'], $orderdetails->payment_order_id,$tokenjsonresponse);
+        $order_price_in_cents = $orderdetails->price * 100 ;
+        $datauser = $this->datauser($integration_id, $order_price_in_cents, $orderdetails->payment_order_id,$tokenjsonresponse);
         $iframe_link = 'https://accept.paymobsolutions.com/api/acceptance/iframes/' . $ifram_id . '?payment_token=' . $datauser['token'];
         return $iframe_link;
 
