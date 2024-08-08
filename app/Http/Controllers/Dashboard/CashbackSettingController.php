5826e587a7be690d8e899e21ef064f30f8fefaa0
AhmedGamalAttia <ahmed.g.attia169@gmail.com>
2024-08-08 12:23:15 +0300

    Insta Order  handle user wallet and cash back  08-08-2024 v1.2

--- app/Http/Controllers/Dashboard/CashbackSettingController.php
+++ app/Http/Controllers/Dashboard/CashbackSettingController.php
@@ -14,6 +14,7 @@ class CashbackSettingController extends Controller
             'cashback_amount' => 0,
             'cashback_percentage' => 0,
             'cashback_limit' => 0,
+            'cashback_type'=>0,
         ];
 
         $settings = GeneralSetting::whereIn('key', array_keys($defaults))->pluck('value', 'key')->toArray();
@@ -27,12 +28,14 @@ class CashbackSettingController extends Controller
     {
         $request->validate([
             'cashback_enabled' => 'required|boolean',
+            'cashback_type' => 'required|boolean',
             'cashback_amount' => 'required|numeric',
             'cashback_percentage' => 'required|numeric',
             'cashback_limit' => 'required|numeric',
         ]);
 
         GeneralSetting::updateOrCreate(['key' => 'cashback_enabled'], ['value' => $request->cashback_enabled]);
+        GeneralSetting::updateOrCreate(['key' => 'cashback_type'], ['value' => $request->cashback_type]);
         GeneralSetting::updateOrCreate(['key' => 'cashback_amount'], ['value' => $request->cashback_amount]);
         GeneralSetting::updateOrCreate(['key' => 'cashback_percentage'], ['value' => $request->cashback_percentage]);
         GeneralSetting::updateOrCreate(['key' => 'cashback_limit'], ['value' => $request->cashback_limit]);
